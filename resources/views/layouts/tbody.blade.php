e26af74453d9c3837ff1b7c60544e8bd32150f48
kevinkresna25 <kevinkresna25@gmail.com>
2024-04-25 23:56:00 +0700

    test txt saja

--- resources/views/layouts/tbody.blade.php
+++ resources/views/layouts/tbody.blade.php
@@ -1,30 +1,4 @@
   <!-- BEGIN VENDOR JS -->
-  <script src="https://my.ubaya.ac.id/assets/plugins/pace/pace.min.js" type="text/javascript"></script>
-  <script src="https://my.ubaya.ac.id/assets/plugins/liga.js" type="text/javascript"></script>
-  <script src="https://my.ubaya.ac.id/assets/plugins/modernizr.custom.js" type="text/javascript"></script>
-  <script src="https://my.ubaya.ac.id/assets/plugins/jquery-ui/jquery-ui.min.js" type="text/javascript"></script>
-  <script src="https://my.ubaya.ac.id/assets/plugins/popper/umd/popper.min.js" type="text/javascript"></script>
-  <script src="https://my.ubaya.ac.id/assets/plugins/bootstrap/js/bootstrap.min.js" type="text/javascript"></script>
-  <script src="https://my.ubaya.ac.id/assets/plugins/jquery/jquery-easy.js" type="text/javascript"></script>
-  <script src="https://my.ubaya.ac.id/assets/plugins/jquery/jquery.serializejson.js" type="text/javascript"></script>
-  <script src="https://my.ubaya.ac.id/assets/plugins/jquery-unveil/jquery.unveil.min.js" type="text/javascript"></script>
-  <script src="https://my.ubaya.ac.id/assets/plugins/jquery-ios-list/jquery.ioslist.min.js" type="text/javascript"></script>
-  <script src="https://my.ubaya.ac.id/assets/plugins/jquery-actual/jquery.actual.min.js"></script>
-  <script src="https://my.ubaya.ac.id/assets/plugins/jquery-scrollbar/jquery.scrollbar.min.js"></script>
-  <script type="text/javascript" src="https://my.ubaya.ac.id/assets/plugins/select2/js/select2.full.min.js"></script>
-  <script type="text/javascript" src="https://my.ubaya.ac.id/assets/plugins/classie/classie.js"></script>
-  <script type="text/javascript" src="https://my.ubaya.ac.id/assets/plugins/jquery-autonumeric/autoNumeric.js"></script>
-  <script type="text/javascript" src="https://my.ubaya.ac.id/assets/plugins/dropzone/dropzone.min.js"></script>
-  <script type="text/javascript" src="https://my.ubaya.ac.id/assets/plugins/jquery-inputmask/jquery.inputmask.min.js"></script>
-  <script src="https://my.ubaya.ac.id/assets/plugins/bootstrap-form-wizard/js/jquery.bootstrap.wizard.min.js" type="text/javascript"></script>
-  <script src="https://my.ubaya.ac.id/assets/plugins/bootstrap-datepicker/js/bootstrap-datepicker.js" type="text/javascript"></script>
-  <script src="https://my.ubaya.ac.id/assets/plugins/quill/quill.min.js" type="text/javascript"></script>
-  <script src="https://my.ubaya.ac.id/assets/plugins/moment/moment.min.js"></script>
-  <script src="https://my.ubaya.ac.id/assets/plugins/bootstrap-daterangepicker/daterangepicker.js"></script>
-  <script src="https://my.ubaya.ac.id/assets/plugins/bootstrap-timepicker/bootstrap-timepicker.min.js"></script>
-  <script src="https://my.ubaya.ac.id/assets/plugins/bootstrap-typehead/typeahead.bundle.min.js"></script>
-  <script src="https://my.ubaya.ac.id/assets/plugins/bootstrap-typehead/typeahead.jquery.min.js"></script>
-  <script src="https://my.ubaya.ac.id/assets/plugins/handlebars/handlebars-v4.0.5.js"></script>
   <script src="https://my.ubaya.ac.id/assets/plugins/jquery-validation/js/jquery.validate.min.js" type="text/javascript"></script>
   <script src="https://my.ubaya.ac.id/assets/plugins/jquery-validation/js/localization/messages_id.js" type="text/javascript"></script>
   <script src="https://my.ubaya.ac.id/assets/plugins/jquery-datatable/media/js/jquery.dataTables.min.js" type="text/javascript"></script>
