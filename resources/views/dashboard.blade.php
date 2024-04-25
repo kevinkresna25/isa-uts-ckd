f56346ccc58e7dc899e4586c9e17fe9d704d6bc4
kevinkresna25 <kevinkresna25@gmail.com>
2024-04-25 22:43:16 +0700

    Perbarui repository dan .gitignore

---
+++ resources/views/dashboard.blade.php
@@ -0,0 +1,7 @@
+@extends('layouts.main')
+
+@section('content')
+    <p>
+        Selamat datang di SMA Rungkad 01.
+    </p>
+@endsection
---
+++ resources/views/presensi.blade.php
@@ -0,0 +1,7 @@
+@extends('layouts.main')
+
+@section('content')
+    <p>
+        Selamat datang di Presensi.
+    </p>
+@endsection
