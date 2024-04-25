3d24234afc3187539fa10d0953201d7bb8fc3da2
kevinkresna25 <kevinkresna25@gmail.com>
2024-04-25 22:40:33 +0700

    perbarui repository github

--- resources/views/dashboard.blade.php
+++
@@ -1,7 +0,0 @@
-@extends('layouts.main')
-
-@section('content')
-    <p>
-        Selamat datang di SMA Rungkad 01.
-    </p>
-@endsection
--- resources/views/presensi.blade.php
+++
@@ -1,7 +0,0 @@
-@extends('layouts.main')
-
-@section('content')
-    <p>
-        Selamat datang di Presensi.
-    </p>
-@endsection
